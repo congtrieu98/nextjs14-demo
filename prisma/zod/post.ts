29f0dfc1b7aa9a8351887a07c16384842a0b492f
congtrieu98 <trieunguyen2806@gmail.com>
2023-11-28 17:53:01 +0700

    add form post

---
+++ prisma/zod/post.ts
@@ -0,0 +1,24 @@
+import * as z from "zod"
+import { CompleteUser, relatedUserSchema } from "./index"
+
+export const postSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  slug: z.string(),
+  content: z.string().min(0),
+  image: z.string(),
+  userId: z.string(),
+})
+
+export interface CompletePost extends z.infer<typeof postSchema> {
+  user: CompleteUser
+}
+
+/**
+ * relatedPostSchema contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const relatedPostSchema: z.ZodSchema<CompletePost> = z.lazy(() => postSchema.extend({
+  user: relatedUserSchema,
+}))
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,8 +22,9 @@ export default async function Navbar() {
   if (session?.user) {
     return (
       <nav className="py-2 flex items-center justify-between transition-all duration-300">
-        <h1 className="font-semibold hover:opacity-75 transition-hover cursor-pointer">
+        <h1 className="font-semibold hover:opacity-75 transition-hover cursor-pointer space-x-2">
           <Link href="/">Logo</Link>
+          <Link href="/posts">post</Link>
         </h1>
         <div className="space-x-2 flex items-center">
           <ModeToggle />
