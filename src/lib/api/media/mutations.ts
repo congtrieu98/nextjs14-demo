f7205015291cbd525683c3ac2854cf6da152a483
congtrieu98 <trieunguyen2806@gmail.com>
2023-11-30 21:51:23 +0700

    update file queries media

--- src/lib/api/media/mutations.ts
+++ src/lib/api/media/mutations.ts
@@ -1,16 +1,17 @@
 import { db } from "@/lib/db/index";
-import { 
-  MediId, 
+import {
+  MediId,
   NewMediParams,
-  UpdateMediParams, 
+  UpdateMediParams,
   updateMediSchema,
-  insertMediSchema, 
-  mediIdSchema 
+  insertMediSchema,
+  mediIdSchema
 } from "@/lib/db/schema/media";
 
 export const createMedi = async (medi: NewMediParams) => {
   const newMedi = insertMediSchema.parse(medi);
   try {
+    // @ts-ignore
     const m = await db.medi.create({ data: newMedi });
     return { medi: m };
   } catch (err) {
@@ -24,7 +25,8 @@ export const updateMedi = async (id: MediId, medi: UpdateMediParams) => {
   const { id: mediId } = mediIdSchema.parse({ id });
   const newMedi = updateMediSchema.parse(medi);
   try {
-    const m = await db.medi.update({ where: { id: mediId }, data: newMedi})
+    // @ts-ignore
+    const m = await db.medi.update({ where: { id: mediId }, data: newMedi })
     return { medi: m };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
@@ -36,7 +38,8 @@ export const updateMedi = async (id: MediId, medi: UpdateMediParams) => {
 export const deleteMedi = async (id: MediId) => {
   const { id: mediId } = mediIdSchema.parse({ id });
   try {
-    const m = await db.medi.delete({ where: { id: mediId }})
+    // @ts-ignore
+    const m = await db.medi.delete({ where: { id: mediId } })
     return { medi: m };
   } catch (err) {
     const message = (err as Error).message ?? "Error, please try again";
--- src/lib/api/media/queries.ts
+++ src/lib/api/media/queries.ts
@@ -2,11 +2,13 @@ import { db } from "@/lib/db/index";
 import { type MediId, mediIdSchema } from "@/lib/db/schema/media";
 
 export const getMedias = async () => {
+  // @ts-ignore
   const m = await db.medi.findMany({});
   return { media: m };
 };
 
 export const getMediById = async (id: MediId) => {
+  // @ts-ignore
   const { id: mediId } = mediIdSchema.parse({ id });
   const m = await db.medi.findFirst({
     where: { id: mediId }
