6abda6ca9a10afd059585cd8bc66948d56b01bfc
congtrieu98 <trieunguyen2806@gmail.com>
2023-11-30 21:46:33 +0700

    update file queries feed

--- src/lib/api/feed/queries.ts
+++ src/lib/api/feed/queries.ts
@@ -4,6 +4,7 @@ import { type FeeId, feeIdSchema } from "@/lib/db/schema/feed";
 
 export const getFees = async () => {
   const { session } = await getUserAuth();
+  // @ts-ignore
   const f = await db.fee.findMany({ where: { userId: session?.user.id! } });
   return { feed: f };
 };
